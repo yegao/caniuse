3971a07b784c3d4c1e4f868394bdf8b6134c4061
yegao <familyink@163.com>
2018-12-29 20:18:24 +0800

    asdas

--- plugins/caniuse.js
+++ plugins/caniuse.js
@@ -1,6 +1,23 @@
-// import fs from 'fs';
-// import path from 'path'
-import getMap from "./map.js"
+const m2j = require('md2json')
+let cache = [];
+let map = {};
+
+const getApiDetail = async () => {
+    await m2j.scan(__dirname+'/InternetTechDevelop/docs/api',/\.((md)|(markdown))$/i,[],(res)=>{
+        cache.push(res);
+    });
+    
+    for(let one of cache){
+        let str = one.content;
+        str.replace(/\#\#\#(\s*)([\w._$]*)\([^\)]*\)(\s*\`?)(\d*)(\+?\`?)/g,function(a,b,c,d,e){
+           map[c] = {
+               version: +e || 1020
+           }
+        })
+    }
+    return map;
+}
+
 export default () => {
     return {
         // 插件名称
@@ -15,7 +32,7 @@ export default () => {
         load(id){
             console.log("load:",id)
             if(id==='api-document'){
-                return getMap().then(function(res){
+                return getApiDetail().then(function(res){
                     return "export default "+JSON.stringify(res)
                 })
             }
